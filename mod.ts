14b3794eba1e71bb2bb3d4f542402e8d071a7b96
Panth <panth977@Panths-MacBook-Pro.local>
2024-11-23 03:04:04 +0530

    init

---
+++ mod.ts
@@ -0,0 +1,512 @@
+import type {
+  RedisClientType,
+  RedisDefaultModules,
+  RedisFunctions,
+  RedisModules,
+  RedisScripts,
+} from "redis";
+import { CACHE } from "@panth977/cache";
+import type { FUNCTIONS } from "@panth977/functions";
+import { TOOLS } from "@panth977/tools";
+
+function time() {
+  const start = Date.now();
+  return function () {
+    return Date.now() - start;
+  };
+}
+
+export function decode<T>(val: string): T {
+  return JSON.parse(val);
+}
+export function encode<T>(val: T): string {
+  return JSON.stringify(val);
+}
+
+export const luaScripts: {
+  readonly exists: string;
+  readonly read: string;
+  readonly write: string;
+  readonly remove: string;
+  readonly increment: string;
+} = {
+  exists: `
+local result = {}
+local null = cjson.decode('null')
+for i, key in ipairs(KEYS) do
+  local fields = cjson.decode(ARGV[i])
+  local keyType = redis.call('type', key).ok
+  if keyType == 'string' and fields == null then
+    table.insert(result, 1)
+  elseif keyType == 'hash' and fields == '*' then
+    local hashResult = {}
+    for _, field in ipairs(redis.call('hkeys', KEYS[1])) do
+      table.insert(hashResult, field)
+      table.insert(hashResult, 1)
+    end
+    table.insert(result, hashResult)
+  elseif keyType == 'hash' and fields ~= null and fields ~= '*' then
+    local hashResult = {}
+    for _, field in ipairs(fields) do
+      table.insert(hashResult, field)
+      table.insert(hashResult, redis.call('hexists', key, field))
+    end
+    table.insert(result, hashResult)
+  else
+    table.insert(result, 0)
+  end
+end
+return result
+  `,
+  read: `
+local result = {}
+local null = cjson.decode('null')
+for i, key in ipairs(KEYS) do
+  local fields = cjson.decode(ARGV[i])
+  local keyType = redis.call('type', key).ok
+  if keyType == 'string' and fields == null then
+    table.insert(result, redis.call('get', key))
+  elseif keyType == 'hash' and fields == '*' then
+    table.insert(result, redis.call('hgetall', key))
+  elseif keyType == 'hash' and fields ~= null and fields ~= '*' then
+    local hashResult = {}
+    for _, field in ipairs(fields) do
+      table.insert(hashResult, field)
+      table.insert(hashResult, redis.call('hget', key, field))
+    end
+    table.insert(result, hashResult)
+  else
+    table.insert(result, null)
+  end
+end
+return result
+  `,
+  write: `
+for i, key in ipairs(KEYS) do
+  local pairOfValAndExp = cjson.decode(ARGV[i])
+  local value = pairOfValAndExp[1]
+  local exp = pairOfValAndExp[2]
+  local keyType = redis.call('type', key).ok
+
+  if type(value) == "string" then
+    if keyType ~= 'string' and keyType ~= 'none' then 
+      redis.call('del', key)
+    end
+    redis.call('set', key, value)
+    if exp > 0 then
+      redis.call('expire', key, exp)
+    end
+
+  elseif type(value) == "table" then
+    if keyType ~= 'hash' and keyType ~= 'none' then 
+      redis.call('del', key)
+    end
+    for field, fieldValue in pairs(value) do
+      redis.call('hset', key, field, fieldValue)
+    end
+    if exp > 0 then 
+      redis.call('expire', key, exp)
+    end
+  end
+end
+  `,
+  remove: `
+local null = cjson.decode('null')
+for i, key in ipairs(KEYS) do
+  local fields = cjson.decode(ARGV[i])
+  local keyType = redis.call('type', key).ok
+  if keyType == 'string' and fields == null then
+    redis.call('del', key)
+  elseif keyType == 'hash' and fields == '*' then
+    redis.call('del', key)
+  elseif keyType == 'hash' and fields ~= null and fields ~= '*' then
+    for _, field in ipairs(fields) do
+      redis.call('hdel', key, field)
+    end
+  end
+end
+return 1
+  `,
+  increment: `
+local key = KEYS[1]
+local incrBy = tonumber(ARGV[1])
+local maxLimit = tonumber(ARGV[2])
+local expirySec = tonumber(ARGV[3])
+local currentValue = tonumber(redis.call('GET', key) or '0')
+local allowed = 1
+if maxLimit and maxLimit > 0 and currentValue + incrBy > maxLimit then
+  allowed = 0
+end
+if allowed == 1 then
+  currentValue = redis.call('INCRBY', key, incrBy)
+  if expirySec and expirySec > 0 then
+    redis.call('EXPIRE', key, expirySec)
+  end
+end
+return {allowed, currentValue}
+  `,
+};
+export type AllFields = typeof CACHE.AbstractCacheClient.AllFields;
+/**
+ * Use this as a client for CACHE.CacheController
+ */ export class RedisCacheClient<
+  M extends RedisModules = RedisDefaultModules,
+  F extends RedisFunctions = Record<string, never>,
+  S extends RedisScripts = Record<string, never>
+> extends CACHE.AbstractCacheClient {
+  readonly client: RedisClientType<M, F, S>;
+  readonly existsExe: (
+    cmd: [string, string[] | "*" | undefined]
+  ) => Promise<boolean | Record<string, boolean> | null>;
+  readonly readExe: (
+    cmd: [string, string[] | "*" | undefined]
+  ) => Promise<string | Record<string, string> | null>;
+  readonly writeExe: (
+    cmd: [string, string | Record<string, string>, number]
+  ) => Promise<void>;
+  readonly removeExe: (
+    cmd: [string, string[] | "*" | undefined]
+  ) => Promise<void>;
+  readonly opt: {
+    decode: <T>(val: string) => T;
+    encode: <T>(val: T) => string;
+    delayInMs: number;
+    label: string;
+  };
+
+  constructor(
+    client: RedisClientType<M, F, S>,
+    opt_?: {
+      label?: string;
+      delayInMs?: number;
+      decode?: <T>(val: string) => T;
+      encode?: <T>(val: T) => string;
+    }
+  ) {
+    const opt = {
+      label: opt_?.label ?? "Redis",
+      decode: opt_?.decode ?? decode,
+      delayInMs: opt_?.delayInMs ?? 0,
+      encode: opt_?.encode ?? encode,
+    };
+    super(opt.label);
+    this.client = client;
+    this.opt = opt;
+    const timeout = opt.delayInMs;
+    this.existsExe = TOOLS.CreateBatchProcessor(timeout, async function (cmds) {
+      const result = await client.eval(luaScripts.exists, {
+        keys: cmds.map((p) => p[0]),
+        arguments: cmds.map((p) => JSON.stringify(p[1] || null)),
+      });
+      const values = [];
+      for (const item of result as (string | string[])[]) {
+        if (typeof item === "number" || typeof item === "string") {
+          values.push(!!+item);
+        } else if (Array.isArray(item)) {
+          const obj: Record<string, boolean> = {};
+          for (let i = 0; i < item.length; i += 2)
+            obj[item[i]] = !!+item[i + 1];
+          values.push(obj);
+        } else {
+          values.push(null);
+        }
+      }
+      return values;
+    });
+    this.readExe = TOOLS.CreateBatchProcessor(timeout, async function (cmds) {
+      const result = await client.eval(luaScripts.read, {
+        keys: cmds.map((p) => p[0]),
+        arguments: cmds.map((p) => JSON.stringify(p[1] || null)),
+      });
+      const values = [];
+      for (const item of result as (string | string[])[]) {
+        if (typeof item === "string") {
+          values.push(item);
+        } else if (Array.isArray(item)) {
+          const obj: Record<string, string> = {};
+          for (let i = 0; i < item.length; i += 2) obj[item[i]] = item[i + 1];
+          values.push(obj);
+        } else {
+          values.push(null);
+        }
+      }
+      return values;
+    });
+    this.writeExe = TOOLS.CreateBatchProcessor(timeout, async function (cmds) {
+      await client.eval(luaScripts.write, {
+        keys: cmds.map((x) => x[0]),
+        arguments: cmds.map((x) => [x[1], x[2]]).map((x) => JSON.stringify(x)),
+      });
+      return Array(cmds.length);
+    });
+    this.removeExe = TOOLS.CreateBatchProcessor(timeout, async function (cmds) {
+      await client.eval(luaScripts.remove, {
+        keys: cmds.map((p) => p[0]),
+        arguments: cmds.map((p) => JSON.stringify(p[1] || null)),
+      });
+      return Array(cmds.length);
+    });
+  }
+  override async existsKey(
+    context: FUNCTIONS.Context,
+    key: CACHE.KEY,
+    log?: boolean
+  ): Promise<boolean> {
+    const timer = time();
+    let Err = undefined;
+    const value = await this.existsExe([key.toString(), undefined]).catch(
+      (err) => {
+        Err = err ?? null;
+        return false;
+      }
+    );
+    if (log) {
+      context.log(
+        `(${timer()} ms) ${this.name}.exists(${key}) ${
+          Err === undefined ? `✅` : `❌: ${Err}`
+        }`
+      );
+    }
+    if (value === null || typeof value !== "boolean") return false;
+    return value;
+  }
+  override async existsHashFields(
+    context: FUNCTIONS.Context,
+    key: CACHE.KEY,
+    fields: CACHE.KEY[] | AllFields,
+    log?: boolean
+  ): Promise<Record<string, boolean>> {
+    const timer = time();
+    let Err = undefined;
+    const value = await this.existsExe([
+      key.toString(),
+      fields === CACHE.AbstractCacheClient.AllFields
+        ? "*"
+        : fields.map((x) => x.toString()),
+    ]).catch((err) => {
+      Err = err ?? null;
+      return {};
+    });
+    if (log) {
+      context.log(
+        `(${timer()} ms) ${this.name}.exists(${key}, ${
+          fields === CACHE.AbstractCacheClient.AllFields ? "*" : `[${fields}]`
+        }) ${Err === undefined ? `✅` : `❌: ${Err}`}`
+      );
+    }
+    if (value === null || typeof value === "boolean") return {};
+    return value;
+  }
+
+  override async readKey<T>(
+    context: FUNCTIONS.Context,
+    key: CACHE.KEY,
+    log?: boolean
+  ): Promise<T | undefined> {
+    const timer = time();
+    let Err = undefined;
+    const value = await this.readExe([key.toString(), undefined]).catch(
+      (err) => {
+        Err = err ?? null;
+        return null;
+      }
+    );
+    if (log) {
+      context.log(
+        `(${timer()} ms) ${this.name}.read(${key}) ${
+          Err === undefined ? `✅` : `❌: ${Err}`
+        }`
+      );
+    }
+    if (value === null || typeof value !== "string") return undefined;
+    return this.opt.decode(value);
+  }
+  override async readHashFields<T extends Record<string, unknown>>(
+    context: FUNCTIONS.Context,
+    key: CACHE.KEY,
+    fields: AllFields | CACHE.KEY[],
+    log?: boolean
+  ): Promise<Partial<T>> {
+    const timer = time();
+    let Err = undefined;
+    const value = await this.readExe([
+      key.toString(),
+      fields === CACHE.AbstractCacheClient.AllFields
+        ? "*"
+        : fields.map((x) => x.toString()),
+    ]).catch((err) => {
+      Err = err ?? null;
+      return {} as Record<string, string>;
+    });
+    if (log) {
+      context.log(
+        `(${timer()} ms) ${this.name}.read(${key}, ${
+          fields === CACHE.AbstractCacheClient.AllFields ? "*" : `[${fields}]`
+        }) ${Err === undefined ? `✅` : `❌: ${Err}`}`
+      );
+    }
+    if (value === null || typeof value === "string") return {};
+    const ret: Partial<T> = {};
+    for (const key in value) {
+      if (typeof value[key] === "string") {
+        (ret as any)[key] = this.opt.decode(value[key]);
+      }
+    }
+    return ret;
+  }
+
+  override async writeKey<T>(
+    context: FUNCTIONS.Context,
+    key: CACHE.KEY,
+    value: T | Promise<T>,
+    expire: number,
+    log?: boolean
+  ): Promise<void> {
+    let awaitedValue: T;
+    try {
+      awaitedValue = await value;
+      if (awaitedValue === undefined) return;
+    } catch {
+      return;
+    }
+    const timer = time();
+    let Err = undefined;
+    const val = this.opt.encode(awaitedValue);
+    await this.writeExe([key.toString(), val, expire]).catch((err) => {
+      Err = err ?? null;
+    });
+    if (log) {
+      context.log(
+        `(${timer()} ms) ${this.name}.write(${key}) ${
+          Err === undefined ? `✅` : `❌: ${Err}`
+        }`
+      );
+    }
+  }
+  override async writeHashFields<T extends Record<string, unknown>>(
+    context: FUNCTIONS.Context,
+    key: CACHE.KEY,
+    value: T | Promise<T>,
+    expire: number,
+    log?: boolean
+  ): Promise<void> {
+    let awaitedValue: T;
+    try {
+      awaitedValue = await value;
+      for (const key in awaitedValue) {
+        if (awaitedValue[key] === undefined) {
+          delete awaitedValue[key];
+        }
+      }
+      if (!Object.keys(awaitedValue).length) return;
+    } catch {
+      return;
+    }
+    const timer = time();
+    let Err = undefined;
+    await this.writeExe([
+      key.toString(),
+      Object.fromEntries(
+        Object.keys(awaitedValue).map((key) => [
+          key,
+          this.opt.encode(awaitedValue[key]),
+        ])
+      ),
+      expire,
+    ]).catch((err) => {
+      Err = err ?? null;
+    });
+    if (log) {
+      context.log(
+        `(${timer()} ms) ${this.name}.read(${key}, [${
+          //
+          Object.keys(awaitedValue)
+        }]) ${Err === undefined ? `✅` : `❌: ${Err}`}`
+      );
+    }
+  }
+
+  override async removeKey(
+    context: FUNCTIONS.Context,
+    key: CACHE.KEY,
+    log?: boolean
+  ): Promise<void> {
+    const timer = time();
+    let Err = undefined;
+    await this.removeExe([key.toString(), undefined]).catch((err) => {
+      Err = err ?? null;
+    });
+    if (log) {
+      context.log(
+        `(${timer()} ms) ${this.name}.remove(${key}) ${
+          Err === undefined ? `✅` : `❌: ${Err}`
+        }`
+      );
+    }
+  }
+  override async removeHashFields(
+    context: FUNCTIONS.Context,
+    key: CACHE.KEY,
+    fields: AllFields | CACHE.KEY[],
+    log?: boolean
+  ): Promise<void> {
+    const timer = time();
+    let Err = undefined;
+    await this.removeExe([
+      key.toString(),
+      fields === CACHE.AbstractCacheClient.AllFields
+        ? "*"
+        : fields.map((x) => x.toString()),
+    ]).catch((err) => {
+      Err = err ?? null;
+    });
+    if (log) {
+      context.log(
+        `(${timer()} ms) ${this.name}.read(${key}, ${
+          fields === CACHE.AbstractCacheClient.AllFields ? "*" : `[${fields}]`
+        }) ${Err === undefined ? `✅` : `❌: ${Err}`}`
+      );
+    }
+  }
+
+  async increment(
+    context: FUNCTIONS.Context,
+    controller: CACHE.CacheController | null,
+    params: { key: string; incrBy: number; maxLimit?: number; expiry?: number }
+  ): Promise<{ allowed: boolean; value: number }> {
+    if (controller) {
+      if (controller.client !== this)
+        throw new Error("Invalid usage of Controller!");
+      params.key = controller.getKey(params.key);
+      if (!controller.can("increment")) return { allowed: false, value: 0 };
+    }
+    if (controller && params.expiry === undefined)
+      params.expiry = controller.defaultExpiry;
+    let error: unknown;
+    const start = Date.now();
+    const result = await this.client
+      .eval(luaScripts.increment, {
+        keys: [params.key],
+        arguments: [
+          `${params.incrBy}`,
+          `${params.maxLimit || 0}`,
+          `${params.expiry || 0}`,
+        ],
+      })
+      .catch((err) => {
+        error = err;
+        return { allowed: false, value: 0 };
+      });
+    const timeTaken = Date.now() - start;
+    if (!Array.isArray(result) || result.length !== 2)
+      throw new Error("Unexpected response from Redis script!");
+    log: {
+      const isErr = error !== undefined;
+      if (!isErr && !controller?.log) break log;
+      let query = `(${timeTaken} ms) ++ ${this.name}.incr(${params.incrBy}, max: ${params.maxLimit})`;
+      query += `\n\t.at(${params.key}): ${isErr ? "❌" : "✅"}`;
+      context.log(query, ...(isErr ? [error] : []));
+    }
+    return { allowed: !!result[0], value: +(result[1] as string | number) };
+  }
+}
